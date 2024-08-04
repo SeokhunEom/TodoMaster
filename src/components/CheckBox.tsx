7e1d0989bdab5998a547ed8d0f5af4015215725a
SeokhunEom <tommya98@naver.com>
2024-08-04 11:22:04 +0900

    CheckBox 컴포넌트 구현

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,7 +1,18 @@
-const CheckBox = () => {
+interface CheckBoxProps {
+  checked: boolean
+  onClick: () => void
+}
+
+const CheckBox = ({ checked, onClick }: CheckBoxProps) => {
   return (
-    <div>
-      <input type='checkbox' />
+    <div
+      onClick={onClick}
+      className={`h-6 w-6 rounded-full border-2 ${checked ? 'border-blue bg-blue' : 'border-blue'} hover:border-blue-dark ${checked ? 'hover:bg-blue-dark' : ''}`}>
+      {checked && (
+        <div className='flex h-full w-full items-center justify-center text-white'>
+          ✓
+        </div>
+      )}
     </div>
   )
 }
