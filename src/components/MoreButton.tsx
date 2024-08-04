c3bb05463f19a78026ea419654fde1da91827510
SeokhunEom <tommya98@naver.com>
2024-08-04 17:56:13 +0900

    투두 steps 아코디언 및 로딩 스피너 적용

---
+++ src/components/MoreButton.tsx
@@ -0,0 +1,36 @@
+import type { SVGProps } from 'react';
+
+interface MoreButtonProps {
+  onClick: () => void;
+}
+
+const MoreButton = ({ onClick }: MoreButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className='flex h-6 w-6 items-center justify-center rounded text-gray-300 hover:bg-gray-400 hover:text-blue'>
+      <IconamoonArrowDown2Bold width='1.2em' height='1.2em' />
+    </button>
+  );
+};
+
+export function IconamoonArrowDown2Bold(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='1em'
+      height='1em'
+      viewBox='0 0 24 24'
+      {...props}>
+      <path
+        fill='none'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth={2.5}
+        d='m7 10l5 5m0 0l5-5'></path>
+    </svg>
+  );
+}
+
+export default MoreButton;
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,6 +1,13 @@
+import {
+  ControlledAccordion,
+  AccordionItem,
+  useAccordionProvider,
+} from '@szhsin/react-accordion';
+import PulseLoader from 'react-spinners/PulseLoader';
 import { TodoItemType } from '../store';
 import CheckBox from './CheckBox';
 import DeleteButton from './DeleteButton';
+import MoreButton from './MoreButton';
 
 export interface TodoItemProps extends TodoItemType {
   onCheckBoxClick: () => void;
@@ -8,23 +15,44 @@ export interface TodoItemProps extends TodoItemType {
 }
 
 const TodoItem = ({
+  id,
   checked,
   onCheckBoxClick,
   onDeleteButtonClick,
   todoTitle,
   steps,
 }: TodoItemProps) => {
+  const providerValue = useAccordionProvider({
+    transition: true,
+    transitionTimeout: 250,
+  });
+  const { toggle } = providerValue;
+
   return (
     <div className='flex w-full gap-3 rounded-lg bg-gray-500 p-4'>
       <div className='shrink-0'>
         <CheckBox checked={checked} onClick={onCheckBoxClick} />
       </div>
-      <div
-        className={`${checked ? 'text-gray-300 line-through' : 'text-gray-100'}`}>
-        {todoTitle}
-        {steps?.steps.join(', ')}
-      </div>
-      <div className='ml-auto shrink-0'>
+      <ControlledAccordion className='w-full' providerValue={providerValue}>
+        <AccordionItem
+          itemKey={id}
+          header={todoTitle}
+          className={`${checked ? 'text-gray-300 line-through' : 'text-gray-100'} `}>
+          {steps ? (
+            <ul className='flex flex-col gap-2 py-4'>
+              {steps.steps.map((step, index) => (
+                <li key={index} className='text-gray-100'>
+                  {step}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <PulseLoader className='pt-4' color='#f2f2f2' size={10} />
+          )}
+        </AccordionItem>
+      </ControlledAccordion>
+      <div className='ml-auto flex shrink-0 gap-2'>
+        <MoreButton onClick={() => toggle(id)} />
         <DeleteButton onClick={onDeleteButtonClick} />
       </div>
     </div>
