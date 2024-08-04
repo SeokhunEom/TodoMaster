beb793c2f2d1ad5b3ec8f59fb4ea0f50848120d6
SeokhunEom <tommya98@naver.com>
2024-08-04 12:06:16 +0900

    TodoItem 컴포넌트 구현

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,33 @@
-const TodoItem = () => {
-  return <div>TodoItem</div>
+import CheckBox from './CheckBox'
+import DeleteButton from './DeleteButton'
+
+interface TodoItemProps {
+  checked: boolean
+  onCheckBoxClick: () => void
+  onDeleteButtonClick: () => void
+  todoTitle: string
+}
+
+const TodoItem = ({
+  checked,
+  onCheckBoxClick,
+  onDeleteButtonClick,
+  todoTitle,
+}: TodoItemProps) => {
+  return (
+    <div className='flex w-full gap-3 rounded-lg bg-gray-500 p-4'>
+      <div className='shrink-0'>
+        <CheckBox checked={checked} onClick={onCheckBoxClick} />
+      </div>
+      <div
+        className={`${checked ? 'text-gray-300 line-through' : 'text-gray-100'}`}>
+        {todoTitle}
+      </div>
+      <div className='ml-auto shrink-0'>
+        <DeleteButton onClick={onDeleteButtonClick} />
+      </div>
+    </div>
+  )
 }
 
 export default TodoItem
